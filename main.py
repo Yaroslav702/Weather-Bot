2b0276f29b3930691c1b592d8083e59dcb68c73b
Yaroslav702 <77899310+Yaroslav702@users.noreply.github.com>
2022-02-04 17:28:53 +0200

    security actions

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def help(update, context):
     context.bot.send_message(chat_id=chat.id, text = message)
 
 
-updater = Updater("5087818773:AAHOjeWhI6CB5BV3atz7xQSZOdK3yoCdqt4")
+updater = Updater("")
 dispatcher = updater.dispatcher
 
 
